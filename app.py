112b5529667ba5dfbdd11119b9b3af5185ec3203
HP Nunes <hp.nunes@protonmail.com>
2018-07-14 14:05:09 -0800

    [mv] commit before change branch

--- app.py
+++ app.py
@@ -120,6 +120,20 @@ def sfgrid():
         data = json.load(blog_file)
         return jsonify(data)
 
+@app.route("/api/crimedata/sfBARTstations")
+def sfBARTstations():
+    json_data = os.path.join(app.static_folder, 'sfBARTstations.geojson')
+    with open(json_data) as blog_file:
+        data = json.load(blog_file)
+        return jsonify(data)
+
+@app.route("/api/crimedata/sfBARTlines")
+def sfBARTlines():
+    json_data = os.path.join(app.static_folder, 'sfBARTlines.geojson')
+    with open(json_data) as blog_file:
+        data = json.load(blog_file)
+        return jsonify(data)
+
 
 @app.route("/api/crimedata/2018/theft")
 def crimedata2018theft():
---
+++ static/js/script.js
@@ -0,0 +1,5 @@
+var map = L.map("map").setView([48.85, 2.35], 12);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
